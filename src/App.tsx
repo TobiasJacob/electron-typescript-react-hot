22ff35aaf79107f30c5b6e66db1f64700ffcb162
Tobias Jacob <tobi.jacob@gmx.net>
2020-01-17 23:14:51 +0100

    added redux devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 import rootReducer from './reducers';
 
 import Router from './Router/Router';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 const App = () => (
 	<Provider store={store}>
--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ const createWindow = () => {
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
   BrowserWindow.addDevToolsExtension('/Users/tobiasjacob/Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.4.0_0')
+  BrowserWindow.addDevToolsExtension('/Users/tobiasjacob/Library/Application Support/Google/Chrome/Default/Extensions/lmhkpmbekcpmknklioeibfkpmmfibljd/2.17.0_0')
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
